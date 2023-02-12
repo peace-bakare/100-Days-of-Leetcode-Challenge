1464e4c92467656bd5977946a7ddd401285f41ad
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-12 19:21:44 +0100

    Solution to Question 169 - Majority Element

---
+++ Day5/Q2.java
@@ -0,0 +1,41 @@
+package Day5;
+
+/*
+169. Majority Element
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+ */
+public class Q2{
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        //Stack<Integer> stack = new Stack<Integer>();
+
+        int count = 1;
+        int maxCount = 1;
+        int num = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i - 1]){
+                count++;
+            }else{
+                count = 1;
+            }
+            //maxCount = Math.max(maxCount, count);
+            if(count > maxCount){
+                maxCount = count;
+                num = nums[i - 1];
+            }
+        }
+        //int me = stack.peek();
+        return num;
+    }
+
+    public void Main(String[]args){
+        int [] array1 = {3, 2, 3};
+        int [] array2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println(majorityElement(array1));
+        System.out.println(majorityElement(array2));
+    }
+}
