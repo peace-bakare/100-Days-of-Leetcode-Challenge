8238e031f359cbd477d87025d9e4f6cb0f019876
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-13 23:51:45 +0100

    Max Consecutive Ones

---
+++ Day6/Q2.java
@@ -0,0 +1,22 @@
+package Day6;
+
+/*
+Question 485 - Max Consecutive Ones
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ */
+public class Q2{
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                maxCount = Math.max(maxCount, count);
+            }else{
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
