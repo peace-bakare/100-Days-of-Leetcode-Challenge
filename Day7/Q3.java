c8b51f5a1e56aedad8fc230cca2724508fe36180
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-16 00:02:10 +0100

    Solution to Question 217 - Contains Duplicates

---
+++ Day7/Q3.java
@@ -0,0 +1,27 @@
+package Day7;
+
+import java.util.Arrays;
+
+
+/*
+ * Question 217 - Contains Duplicate
+ * 
+ * Given an integer array nums, return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.
+ * 
+ * Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+ */
+public class Q3{
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
