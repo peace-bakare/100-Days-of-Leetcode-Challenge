3aee483096caaca63983b086dadb67f928410cb7
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-19 22:30:59 +0100

    Fixed the bug in the code

--- Day4/Q1.java
+++ Day4/Q1.java
@@ -1,13 +1,18 @@
-Package Day4;
+package Day4;
 
-/* Given a non-empty array of integers nums, every element appears twice except for one. 
+import java.util.HashSet;
+import java.util.Iterator;
+
+/* 
+Question 136: Single Number
+Given a non-empty array of integers nums, every element appears twice except for one. 
 Find that single one.
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 
  */
 
-public Q1{
+public class Q1{
     public int singleNumber(int[] nums) {
         HashSet<Integer> hashSet = new HashSet<Integer>();
 
