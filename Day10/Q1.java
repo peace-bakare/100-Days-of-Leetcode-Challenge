e17482af084024d2f5fd0bfb718f59fe2cbfccea
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-19 23:22:38 +0100

    Solution to Question 283 - Move Zeroes

---
+++ Day10/Q1.java
@@ -0,0 +1,34 @@
+package Day10;
+
+
+/*
+Question 283 - Move Zeroes
+
+ * Given an integer array nums, move all 0's to the end of it while maintaining the 
+ * relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ */
+public class Q1 {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        int temp = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
