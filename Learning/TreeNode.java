ea76c27744e958f7628832ae876c5e4fd8aeab67
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-25 22:40:08 +0100

    TreeNode

---
+++ Learning/TreeNode.java
@@ -0,0 +1,10 @@
+public class TreeNode {
+    int val;
+    TreeNode _left;
+    TreeNode _right;
+
+    public TreeNode(TreeNode left, TreeNode right) {
+        _left = left;
+        _right = right;
+    }
+}
