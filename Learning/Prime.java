f9aec9585875d88f09452e76d3da858217911f2f
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-28 22:30:59 +0100

    Finding the next prime number

---
+++ Learning/Prime.java
@@ -0,0 +1,35 @@
+public class Prime {
+    boolean isPrime(int n)
+    {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+
+        if (n % 2 == 0 || n % 3 == 0) return false;
+         
+        for (int i = 5; i * i <= n; i = i + 6){
+            if (n % i == 0 || n % (i + 2) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+     
+    int nextPrime(int N)
+    {
+        if (N <= 1)
+            return 2;
+     
+        int prime = N;
+        boolean found = false;
+     
+        while (!found)
+        {
+            prime++;
+     
+            if (isPrime(prime))
+                found = true;
+        }
+     
+        return prime;
+    }
+}
