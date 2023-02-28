93e5e6eb47dc46f78020e32dc3a26d4d7540bf88
Peace Bakare <Peace.Bakare@sterling.ng>
2023-02-28 22:27:49 +0100

    Solution to Question 342 - Power of Four

---
+++ Day14/Q1.java
@@ -0,0 +1,34 @@
+package Day14;
+
+/*
+ * Question 342 - Power of Four
+ * 
+ * Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ */
+public class Q1 {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0){
+            return false;
+        }
+        
+        while(n % 4 == 0){
+            n /= 4;
+        }
+
+        if(n == 1){
+            return true;
+        }else{
+            return false;
+        }        
+
+        // if(n <= 0){
+        //     return false;
+        // }
+        
+        // boolean x = ((n & (n - 1)) == 0);
+        // return x;
+    }  
+}
