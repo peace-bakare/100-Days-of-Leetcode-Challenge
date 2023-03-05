b9943812105c32d024da169143711478f3cdd450
Peace Bakare <Peace.Bakare@sterling.ng>
2023-03-05 10:00:45 +0100

    Solution to Question 13 - Roman to Integer

---
+++ Day17/Q1.java
@@ -0,0 +1,54 @@
+package Day17;
+
+/*
+ * Question 13 - Roman To Integer
+ * 
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+ */
+import java.util.HashMap;
+
+public class Q1 {
+    public int romanToInt(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int value = 0;
+        int sum = 0;
+
+        for(int i=s.length()-1; i>=0; i--){
+            value = map.get(s.charAt(i));
+            if((4 * value) < sum){
+                sum -= value;
+            }else{
+                sum += value;
+            }
+        }
+
+        for(int i=0, j=0; i<s.length()-1; i++){
+
+        }
+        return sum;
+    }
+}
