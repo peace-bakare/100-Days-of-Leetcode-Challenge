9f7f5c5a1ea47cb89eb6a9e1fd694f2cf640eec6
Peace Bakare <peaceultimatum41@outlook.com>
2023-03-09 23:55:36 +0100

    Solution to Question 709 - To Lower Case

---
+++ Day22/Q2.java
@@ -0,0 +1,25 @@
+package Day22;
+
+/*
+ * Question 709 - To Lower Case
+ * 
+ * Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+
+ */
+public class Q2 {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
