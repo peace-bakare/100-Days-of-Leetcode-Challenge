cc0e31c1c7574c8be8c6e568c7d8ebb677215e44
Peace Bakare <peaceultimatum41@outlook.com>
2023-03-14 12:07:54 +0100

    Solution to Question 1672 - Richest Customer Wealth

---
+++ Day23/Q2.java
@@ -0,0 +1,34 @@
+package Day23;
+
+/*
+ * Question 1672 - Richest Customer Wealth
+ * 
+ * You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer 
+ * has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the 
+customer that has the maximum wealth.
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+ */
+public class Q2 {
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for(int i=0; i<accounts.length; i++){
+            for(int j=0; j<accounts[i].length; j++){
+                sum += accounts[i][j];
+            }
+            max = Math.max(max, sum);
+            sum = 0;
+        }
+        return max;
+    }
+}
